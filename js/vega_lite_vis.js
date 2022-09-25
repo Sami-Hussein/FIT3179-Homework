01d1b7e075b27a01119027d31eb2afc9f6de572d
Sami <shus0013@student.monash.edu>
2022-09-25 14:32:21 +0400

    fixing minor bugs and issues

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "./homework_map.vg.json";
+vegaEmbed("#homework", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
