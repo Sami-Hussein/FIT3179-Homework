7cdf5c11f1cca05328fcff80b8c9cf88914fee80
Sami <shus0013@student.monash.edu>
2022-09-25 14:42:15 +0400

    chart now loads correctly in live server

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "./homework_map.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/Sami-Hussein/FIT3179-Homework/main/js/homework_map.vg";
 vegaEmbed("#homework", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
