19596fb1aad84212196915a94ef518f308dffa87
Sami <shus0013@student.monash.edu>
2022-10-09 15:21:01 +0400

    add week 10 homework files

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,8 @@
 var vg_1 = "https://raw.githubusercontent.com/Sami-Hussein/FIT3179-Homework/main/js/homework_map.vg";
+var vg_2 = "https://raw.githubusercontent.com/Sami-Hussein/FIT3179-Homework/main/js/homework_bar.vg"
 vegaEmbed("#homework", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#barchart", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
